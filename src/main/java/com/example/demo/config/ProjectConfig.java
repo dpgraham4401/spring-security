c186f4a9c7b3a8b3535cbc62afc52051163977f0
David Graham <dpgraham4401@gmail.com>
2024-06-27 15:14:14 -0400

    add password encoder (NoOpPasswordEncoder is marked as deprecated b/c it shouldn't be used in production)
    we also add a user that we can use

--- src/main/java/com/example/demo/config/ProjectConfig.java
+++ src/main/java/com/example/demo/config/ProjectConfig.java
@@ -2,7 +2,10 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -10,6 +13,16 @@ public class ProjectConfig {
 
     @Bean
     UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager();
+        var user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
