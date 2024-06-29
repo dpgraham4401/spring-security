ad906721023f846bec972c1047591c6d59ce486e
David Paul Graham <dpgraham4401@gmail.com>
2024-06-28 20:18:23 -0400

    use BCryptPasswordEncoder and adjust data in initial data.sql

--- src/main/java/com/example/demo/config/UserManagementConfig.java
+++ src/main/java/com/example/demo/config/UserManagementConfig.java
@@ -3,7 +3,7 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
@@ -24,7 +24,7 @@ public class UserManagementConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,7 @@
 INSERT INTO foo.users(id, username, password)
-VALUES ('4e97c051-76ef-40c9-812b-cb064abc437c', 'john', '12345')
-ON CONFLICT DO NOTHING;
-INSERT INTO foo.users(id, username, password)
-VALUES ('e33fb593-7497-4983-bbe4-7df7f22ad331', 'david', 'password')
+VALUES ('4e97c051-76ef-40c9-812b-cb064abc437c', 'john', '$2a$10$bh59wmSe3rIc2uKGd3hJPeDVFNgJx9hnaCUUdsqSwwwppNEy6Anze')
 ON CONFLICT DO NOTHING;
 
 INSERT INTO foo.authorities(user_id, authority)
 VALUES ('4e97c051-76ef-40c9-812b-cb064abc437c', 'read')
-ON CONFLICT DO NOTHING;
-INSERT INTO foo.authorities(user_id, authority)
-VALUES ('e33fb593-7497-4983-bbe4-7df7f22ad331', 'write')
 ON CONFLICT DO NOTHING;
