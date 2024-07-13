bdf846abb0da61ef9596a0a7e31702bae7ed838a
David Paul Graham <dpgraham4401@gmail.com>
2024-07-12 22:35:41 -0400

    simple authorization example that requires users to have 'WRITE' to access the hello endpoint. We have users with WRITE+READ authorities and one with only READ authority
    see listing 7.5

--- src/main/java/com/example/demo/config/AuthConfig.java
+++ src/main/java/com/example/demo/config/AuthConfig.java
@@ -3,10 +3,13 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity // This is needed to enable method-level security
 public class AuthConfig {
 
 
@@ -20,15 +23,15 @@ public class AuthConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
 
-        // Here we're using a form for login instead of the (native) HTTP Basic we had before
-        // We also, on authentication success, redirect the user to the "/home" URL
-        http.formLogin(c -> c.defaultSuccessUrl("/home", true));
-
-        // Nothing new there in this commit -> all requests must be authenticated
-        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
-
         // Nothing new there in this commit -> we're hooking up our custom authentication provider
         http.authenticationProvider(authenticationProvider);
+
+        // Back to implementing HTTP Basic authentication for this Authorization Chapter examples
+        http.httpBasic(Customizer.withDefaults());
+
+        // This reads as: "Any request must be authenticated with a user that has the 'WRITE' authority"
+        // Note: Authorities are case-sensitive
+        http.authorizeHttpRequests(c -> c.anyRequest().hasAnyAuthority("WRITE"));
 
         return http.build();
     }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,20 @@
+-- user john with password 12345
 INSERT INTO foo.users(id, username, password)
 VALUES ('4e97c051-76ef-40c9-812b-cb064abc437c', 'john', '$2a$10$bh59wmSe3rIc2uKGd3hJPeDVFNgJx9hnaCUUdsqSwwwppNEy6Anze')
 ON CONFLICT DO NOTHING;
 
+-- user david with password 12345
+INSERT INTO foo.users(id, username, password)
+VALUES ('7bb77e1a-c95c-4e29-bd8f-74b1204895a6', 'david', '$2a$10$bh59wmSe3rIc2uKGd3hJPeDVFNgJx9hnaCUUdsqSwwwppNEy6Anze')
+ON CONFLICT DO NOTHING;
+
+-- give john read and write permissions
 INSERT INTO foo.authorities(user_id, authority)
-VALUES ('4e97c051-76ef-40c9-812b-cb064abc437c', 'read')
+VALUES ('4e97c051-76ef-40c9-812b-cb064abc437c', 'READ'),
+       ('4e97c051-76ef-40c9-812b-cb064abc437c', 'WRITE')
+ON CONFLICT DO NOTHING;
+
+-- give david read permission
+INSERT INTO foo.authorities(user_id, authority)
+VALUES ('7bb77e1a-c95c-4e29-bd8f-74b1204895a6', 'READ')
 ON CONFLICT DO NOTHING;
