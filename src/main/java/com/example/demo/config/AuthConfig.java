a4b848e4dabbf0dbd70f21dec329782e68f3cfd2
David Paul Graham <dpgraham4401@gmail.com>
2024-07-12 20:26:07 -0400

    reimplement Login form authentication with a homepage

--- src/main/java/com/example/demo/config/AuthConfig.java
+++ src/main/java/com/example/demo/config/AuthConfig.java
@@ -5,17 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class AuthConfig {
 
 
-    private final StaticCalloutFilter filter;
     private final AuthenticationProvider authenticationProvider;
 
-    public AuthConfig(StaticCalloutFilter filter, AuthenticationProvider authenticationProvider) {
-        this.filter = filter;
+    public AuthConfig(AuthenticationProvider authenticationProvider) {
         this.authenticationProvider = authenticationProvider;
     }
 
@@ -23,11 +20,15 @@ public class AuthConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
 
-        http.addFilterAt(filter, BasicAuthenticationFilter.class)
-                .authorizeHttpRequests(c -> c.anyRequest().permitAll());
+        // Here we're using a form for login instead of the (native) HTTP Basic we had before
+        // We also, on authentication success, redirect the user to the "/home" URL
+        http.formLogin(c -> c.defaultSuccessUrl("/home", true));
 
+        // Nothing new there in this commit -> all requests must be authenticated
+        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+
+        // Nothing new there in this commit -> we're hooking up our custom authentication provider
         http.authenticationProvider(authenticationProvider);
-
 
         return http.build();
     }
---
+++ src/main/java/com/example/demo/home/Home.java
@@ -0,0 +1,13 @@
+package com.example.demo.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home.html";
+    }
+}
