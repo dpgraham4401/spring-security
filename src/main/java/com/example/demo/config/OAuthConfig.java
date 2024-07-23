29a9a00aafb0a3526d95c754b47bf544d1c1ab9e
David Paul Graham <dpgraham4401@gmail.com>
2024-07-22 22:09:34 -0400

    OAuth2 server minimal working example
    There's quite a bit going on here. See the OAuthConfig class which defines a beans for SecurityFilterChain (filter OAuth2Authorization Server), RegisteredClientRepository, JWKSource, AuthorizationServerSettings
    
    In reality, We're are not typically going to implement an Authorization
    Server. We rely on other services like Google IAM, AWS Cognito, AUth0,
    or something self hosted

---
+++ src/main/java/com/example/demo/config/OAuthConfig.java
@@ -0,0 +1,120 @@
+package com.example.demo.config;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+@Configuration
+public class OAuthConfig {
+
+    // This bean adds a filter chain that adds OAuth2 Authorization Server support to the application.
+    @Bean
+    // Since we have multiple SecurityFilterChain beans, we need to specify the order of the filter chain.
+    @Order(1)
+    public SecurityFilterChain asFilterChain(HttpSecurity http)
+            throws Exception {
+        // Default configuration for the OAuth2 default endpoints.
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+        // Enable OIDC protocol and Apply the default.
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());
+
+        // I the user is not authenticated, redirect to the login page.
+        http.exceptionHandling((e) ->
+                e.authenticationEntryPoint(
+                        new LoginUrlAuthenticationEntryPoint("/login"))
+        );
+
+        return http.build();
+    }
+
+    @Bean
+    // Again, multiple SecurityFilterChain beans, so we need to specify the order.
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        // Enable form login with defaults ()
+        http.formLogin(Customizer.withDefaults());
+
+        // All requests outside the OAuth2 Authorization Server endpoints require authentication.
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+
+
+    @Bean
+    // A RegisteredClientRepository bean is needed to store the registered clients (RegisteredClient interface).
+    // A RegisteredClientRepo is to OAuth2 client as UserDetailsService is to User (UserDetails interface).
+    public RegisteredClientRepository registeredClientRepository() {
+        // Create a RegisteredClient object with the client details.
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("client")
+                .clientSecret("secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("https://www.manning.com/authorized")
+                .scope(OidcScopes.OPENID)
+                .build();
+
+        // Normally, we would store the registered clients in a database
+        // but for simplicity, we're using an in-memory repository.
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+
+    @Bean
+    // JWKSource manages key-pairs for the OAuth2 Authorization Server.
+    // This is a requirement if we use non-opaque tokens.
+    public JWKSource<SecurityContext> jwkSource() throws NoSuchAlgorithmException {
+        // For this example, we generate new key-pairs every time the application starts.
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAKey rsaKey = new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+
+    @Bean
+    // Last piece in the minimal OAuth2 server configuration.
+    // AuthorizationServerSettings object lets us customize all endpoint paths that the authorization server  exposes.
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder().build();
+    }
+}
--- src/main/java/com/example/demo/product/ProductService.java
+++ src/main/java/com/example/demo/product/ProductService.java
@@ -1,7 +1,5 @@
 package com.example.demo.product;
 
-import org.springframework.security.access.prepost.PostFilter;
-import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,21 +17,7 @@ public class ProductService {
         return productRepository.getProductsById(id);
     }
 
-    // Here we've botched the PostFilter to show that we can use custom attributes of our custom Principal object
-    @PostFilter("filterObject.ownerId == authentication.principal.getId()")
     public List<Product> findSellingProducts() {
         return productRepository.findAllByOrderByName();
-    }
-
-
-    // Here, we use the @PreFilter annotation to remove any product that does not have a matching owner
-    // This is assuming that the input is coming from a trusted source.
-    // If we have more than one parameter (collection), use the filterTarget attribute to specify the parameter to
-    // filter
-    @PreFilter(value = "filterObject.ownerId == authentication.principal.getId()", filterTarget = "products")
-    public void updatePrices(List<Product> products, List<Float> prices) {
-        for (Product product : products) {
-            product.setPrice(product.getPrice() * prices.get(products.indexOf(product)));
-        }
     }
 }
